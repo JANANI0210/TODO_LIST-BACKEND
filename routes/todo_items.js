276e06a778cb441b5bf324e2b571d73e63a9acff
JANANI <jananid101102@gmail.com>
2023-05-30 08:24:37 +0530

    -Update - Included a MVC architecture for GET method

--- routes/todo_items.js
+++ routes/todo_items.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("List of all todo_items");
-});
+const { getALLToDoItems } = require("../controller/todo_items");
+router.route("/").get(getALLToDoItems);
+
 module.exports = router;
