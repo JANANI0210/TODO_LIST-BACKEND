497bec7987121a65fffe834de53e75dc9e4580a1
JANANI <jananid101102@gmail.com>
2023-05-30 08:17:18 +0530

    -Add .Added various methods for REST operations

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("This is the app.js");
+const express = require("express");
+const todo_items = require("./routes/todo_items");
+const app = express();
+const PORT = 3500;
+
+app.get("/", (request, response) => {
+  response.send("Working");
+});
+
+app.use("/api/v1/todo_items", todo_items);
+
+app.listen(PORT, console.log("Server runs at http://localhost:" + PORT));
---
+++ routes/todo_items.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("List of all todo_items");
+});
+module.exports = router;
